f95b07af4fd939cf3edc7d8334f36fff104de4b2
Adi Wahyudi <adi.whyd02@gmail.com>
2025-07-21 14:04:29 +0700

    learn: globals standard library

---
+++ 10-globals.mjs
@@ -0,0 +1,7 @@
+// globals
+// in node.js there are libraries that can be used globally or everywhere without need to import
+
+// example
+setTimeout(() => {
+  console.info("Hello Globals");
+}, 2000);
