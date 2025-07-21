a2818decc262a2332693f9accd35a3c504971135
Adi Wahyudi <adi.whyd02@gmail.com>
2025-07-21 13:11:02 +0700

    learn: web api in node.js

---
+++ 02-web-api.js
@@ -0,0 +1,5 @@
+// this will be error because not supported in node environment
+// remember: not all web api are supported in node environment
+// node.js has own standard library
+// https://nodejs.org/docs/latest/api/
+const request = new XMLHttpRequest();
