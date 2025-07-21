c1c75a6044f1d917d6f489655c157222c3bb96c7
Adi Wahyudi <adi.whyd02@gmail.com>
2025-07-21 14:42:02 +0700

    learn: timers standard library

---
+++ 16-timer.mjs
@@ -0,0 +1,5 @@
+// timer
+// the timer module exposes a global API for scheduling functions to be called at some future period of time.
+setInterval(() => {
+  console.info(`Start time at ${new Date()}`);
+}, 1000);
